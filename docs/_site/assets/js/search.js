7cedf7b6d9d9565fc390250f126bac5159b96f5b
Your Name <you@example.com>
2024-06-21 12:39:26 -0600

    doc updates

--- docs/_site/assets/js/search.js
+++ docs/_site/assets/js/search.js
@@ -1,72 +1,84 @@
-"use strict";
-
 import data from "/sprite/assets/js/searchData.js";
 import serializedIndex from "./serialized.js";
+//import lunr from "lunr"; // assuming lunr is a separate module
 
+// Constants
+const MIN_SEARCH_LENGTH = 3;
+
+// State definition
+const state = {
+  filters: {
+    class: true,
+    method: true,
+    type: true,
+  },
+  results: {
+    filtered: [],
+    full: [],
+  },
+  visible: false,
+};
+
+// Elements for reference
+const elements = {
+  input: null,
+  resultsBox: null,
+  resultList: null,
+  numberOfResults: null,
+  numberResultsShowing: null,
+  backdrop: null,
+  filters: null,
+  searchLabel: null,
+  close: null,
+};
+
+// Initialize search
 async function initializeSearch() {
-  const config = {
-    minSearchLength: 3,
-    indexedFields: ["name", "desc", "type", "parent"],
-  };
+  const root = document.getElementById("searchBox");
+  elements.input = root.querySelector("#searchInput");
+  elements.resultsBox = root.querySelector("#searchResultsBox");
+  elements.resultList = root.querySelector("#resultList");
+  elements.numberOfResults = root.querySelector("#numberOfResults");
+  elements.numberResultsShowing = root.querySelector("#numberResultsShowing");
+  elements.filters = root
+    .querySelector("#searchFilters")
+    .getElementsByTagName("input");
+  elements.searchLabel = root.querySelector("#searchLabel");
+  elements.close = root.querySelector("#searchClose");
 
-  // Build the index using Lunr.js
+  // Load the index
   const index = lunr.Index.load(serializedIndex);
 
-  // Define the root element
-  const root = document.getElementById("searchBox");
-
-  // State definition
-  const state = {
-    filters: {
-      class: true,
-      method: true,
-      type: true,
-    },
-    results: {
-      filtered: [],
-      full: [],
-    },
-    visible: false,
-  };
-
-  // Elements for reference
-  const el = {
-    input: root.querySelector("#searchInput"),
-    results: root.querySelector("#searchResultsBox"),
-    list: root.querySelector("#resultList"),
-    numberOfResults: root.querySelector("#numberOfResults"),
-    numberResultsShowing: root.querySelector("#numberResultsShowing"),
-    backdrop: undefined,
-    filters: root.querySelector("#searchFilters").getElementsByTagName("input"),
-    searchLabel: root.querySelector("#searchLabel"),
-    close: root.querySelector("#searchClose"),
-  };
-
   // Initialize search filter toggles
-  for (let i = 0; i < el.filters.length; ++i) {
-    el.filters[i].addEventListener("change", () => {
-      state.filters[el.filters[i].value] = el.filters[i].checked;
+  for (let i = 0; i < elements.filters.length; ++i) {
+    elements.filters[i].addEventListener("change", () => {
+      state.filters[elements.filters[i].value] = elements.filters[i].checked;
       updateResultList();
     });
   }
 
+  // Add event listeners
+  elements.input.addEventListener("input", search);
+  elements.close.addEventListener("click", hideSearchBox);
+
+  // Define functions
   function appendBackdrop() {
-    el.backdrop = document.createElement("div");
-    el.backdrop.id = "backdrop";
-    el.backdrop.addEventListener("click", hideSearchBox);
-    root.appendChild(el.backdrop);
+    elements.backdrop = document.createElement("div");
+    elements.backdrop.id = "backdrop";
+    elements.backdrop.addEventListener("click", hideSearchBox);
+    root.appendChild(elements.backdrop);
   }
 
   function removeBackdrop() {
-    el.backdrop.removeEventListener("click", appendBackdrop);
-    el.backdrop.remove();
-    el.backdrop = undefined;
+    elements.backdrop.removeEventListener("click", appendBackdrop);
+    elements.backdrop.remove();
+    elements.backdrop = undefined;
   }
 
   function handleKeyboardSelection() {
-    const items = el.list.querySelectorAll("a");
+    const items = elements.list.querySelectorAll("a");
     if (items.length > 0) {
-      if (document.activeElement === el.input) {
+      if (document.activeElement === elements.input) {
         e.preventDefault();
         items[0].focus();
       } else {
@@ -76,7 +88,7 @@ async function initializeSearch() {
             if (i + 1 < items.length) {
               items[i + 1].focus();
             } else {
-              el.input.focus();
+              elements.input.focus();
             }
             break;
           }
@@ -87,7 +99,7 @@ async function initializeSearch() {
 
   function checkKeyPress(e) {
     if (e.key === "Tab") {
-      handleKeyboardSelection();
+      handleKeyboardSelection(e);
     }
     if (e.key === "Escape") {
       hideSearchBox();
@@ -97,27 +109,25 @@ async function initializeSearch() {
   function showResultsBox() {
     appendBackdrop();
     state.visible = true;
-    el.results.classList.remove("hidden");
+    elements.resultsBox.classList.remove("hidden");
     document.addEventListener("keydown", checkKeyPress);
   }
 
   function hideSearchBox() {
     state.visible = false;
-    el.results.classList.add("hidden");
-    document.removeEventListener("keydown", showResultsBox);
+    elements.resultsBox.classList.add("hidden");
+    document.removeEventListener("keydown", checkKeyPress);
     removeBackdrop();
   }
 
   function updateResultList() {
     state.results.filtered = [];
-    el.list.innerHTML = "";
-    el.numberOfResults.innerHTML = state.results.full.length;
-    el.numberResultsShowing.innerHTML = state.results.filtered.length;
+    elements.resultList.innerHTML = "";
+    elements.numberOfResults.innerHTML = state.results.full.length;
+    elements.numberResultsShowing.innerHTML = state.results.filtered.length;
 
     if (state.results.full.length > 0) {
       state.results.full.forEach((result) => {
-        // TODO: should look into this, is it necessary to find
-        // the original document?
         const doc = data.find((doc) => doc.name === result.ref);
         const type = doc.type === "interface" ? "type" : doc.type;
         if (state.filters[type]) {
@@ -143,11 +153,11 @@ async function initializeSearch() {
 
           li.appendChild(section);
 
-          el.list.appendChild(li);
+          elements.resultList.appendChild(li);
         }
       });
 
-      el.numberResultsShowing.innerHTML = state.results.filtered.length;
+      elements.numberResultsShowing.innerHTML = state.results.filtered.length;
     }
   }
 
@@ -158,29 +168,22 @@ async function initializeSearch() {
     updateResultList();
   }
 
-  // search the index
   function search() {
-    // Hide the "Search" Label
-    // (which functions as a placeholder)
-    if (el.input.value.length > 0) {
-      el.searchLabel.classList.add("hidden");
+    if (elements.input.value.length > 0) {
+      elements.searchLabel.classList.add("hidden");
     } else {
-      el.searchLabel.classList.remove("hidden");
+      elements.searchLabel.classList.remove("hidden");
     }
 
-    // run a search if the search term is longer than
-    // the minimum set in the config
-    if (el.input.value.length >= config.minSearchLength) {
-      const initial = index.search(searchInput.value);
+    if (elements.input.value.length >= MIN_SEARCH_LENGTH) {
+      const initial = index.search(elements.input.value);
 
-      // if there are no results, try again with a wildcard,
-      // and then yet again with two wildcards
       if (initial.length === 0) {
-        const wildcardResults = index.search(`${searchInput.value}*`);
+        const wildcardResults = index.search(`${elements.input.value}*`);
         state.results.full =
           wildcardResults.length > 0
             ? wildcardResults
-            : index.search(`*${searchInput.value}*`);
+            : index.search(`*${elements.input.value}*`);
       } else {
         state.results.full = initial;
       }
@@ -188,9 +191,201 @@ async function initializeSearch() {
       displayResults();
     }
   }
-
-  el.input.addEventListener("input", search);
-  el.close.addEventListener("click", hideSearchBox);
 }
 
 document.addEventListener("DOMContentLoaded", initializeSearch);
+
+// import data from "/sprite/assets/js/searchData.js";
+// import serializedIndex from "./serialized.js";
+
+// async function initializeSearch() {
+//   const config = {
+//     minSearchLength: 3,
+//     indexedFields: ["name", "desc", "type", "parent"],
+//   };
+
+//   // Build the index using Lunr.js
+//   const index = lunr.Index.load(serializedIndex);
+
+//   // Define the root element
+//   const root = document.getElementById("searchBox");
+
+//   // State definition
+//   const state = {
+//     filters: {
+//       class: true,
+//       method: true,
+//       type: true,
+//     },
+//     results: {
+//       filtered: [],
+//       full: [],
+//     },
+//     visible: false,
+//   };
+
+//   // Elements for reference
+//   const el = {
+//     input: root.querySelector("#searchInput"),
+//     results: root.querySelector("#searchResultsBox"),
+//     list: root.querySelector("#resultList"),
+//     numberOfResults: root.querySelector("#numberOfResults"),
+//     numberResultsShowing: root.querySelector("#numberResultsShowing"),
+//     backdrop: undefined,
+//     filters: root.querySelector("#searchFilters").getElementsByTagName("input"),
+//     searchLabel: root.querySelector("#searchLabel"),
+//     close: root.querySelector("#searchClose"),
+//   };
+
+//   // Initialize search filter toggles
+//   for (let i = 0; i < el.filters.length; ++i) {
+//     el.filters[i].addEventListener("change", () => {
+//       state.filters[el.filters[i].value] = el.filters[i].checked;
+//       updateResultList();
+//     });
+//   }
+
+//   function appendBackdrop() {
+//     el.backdrop = document.createElement("div");
+//     el.backdrop.id = "backdrop";
+//     el.backdrop.addEventListener("click", hideSearchBox);
+//     root.appendChild(el.backdrop);
+//   }
+
+//   function removeBackdrop() {
+//     el.backdrop.removeEventListener("click", appendBackdrop);
+//     el.backdrop.remove();
+//     el.backdrop = undefined;
+//   }
+
+//   function handleKeyboardSelection() {
+//     const items = el.list.querySelectorAll("a");
+//     if (items.length > 0) {
+//       if (document.activeElement === el.input) {
+//         e.preventDefault();
+//         items[0].focus();
+//       } else {
+//         for (let i = 0; i < results.length; i++) {
+//           if (items[i] === document.activeElement) {
+//             e.preventDefault();
+//             if (i + 1 < items.length) {
+//               items[i + 1].focus();
+//             } else {
+//               el.input.focus();
+//             }
+//             break;
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   function checkKeyPress(e) {
+//     if (e.key === "Tab") {
+//       handleKeyboardSelection();
+//     }
+//     if (e.key === "Escape") {
+//       hideSearchBox();
+//     }
+//   }
+
+//   function showResultsBox() {
+//     appendBackdrop();
+//     state.visible = true;
+//     el.results.classList.remove("hidden");
+//     document.addEventListener("keydown", checkKeyPress);
+//   }
+
+//   function hideSearchBox() {
+//     state.visible = false;
+//     el.results.classList.add("hidden");
+//     document.removeEventListener("keydown", showResultsBox);
+//     removeBackdrop();
+//   }
+
+//   function updateResultList() {
+//     state.results.filtered = [];
+//     el.list.innerHTML = "";
+//     el.numberOfResults.innerHTML = state.results.full.length;
+//     el.numberResultsShowing.innerHTML = state.results.filtered.length;
+
+//     if (state.results.full.length > 0) {
+//       state.results.full.forEach((result) => {
+//         // TODO: should look into this, is it necessary to find
+//         // the original document?
+//         const doc = data.find((doc) => doc.name === result.ref);
+//         const type = doc.type === "interface" ? "type" : doc.type;
+//         if (state.filters[type]) {
+//           state.results.filtered.push(result.ref);
+//           const section = document.createElement("section");
+//           const desc = document.createElement("p");
+//           const link = document.createElement("a");
+//           const li = document.createElement("li");
+
+//           li.className = doc.type;
+
+//           link.href = `/sprite/doc/${doc.name}`;
+//           link.className = "searchResultLink";
+//           link.addEventListener("click", () => {});
+//           link.textContent = doc.parent
+//             ? `${doc.parent}.${doc.name}`
+//             : doc.name;
+
+//           desc.textContent = doc.desc;
+
+//           section.appendChild(link);
+//           section.appendChild(desc);
+
+//           li.appendChild(section);
+
+//           el.list.appendChild(li);
+//         }
+//       });
+
+//       el.numberResultsShowing.innerHTML = state.results.filtered.length;
+//     }
+//   }
+
+//   function displayResults() {
+//     if (!state.visible) {
+//       showResultsBox();
+//     }
+//     updateResultList();
+//   }
+
+//   // search the index
+//   function search() {
+//     // Hide the "Search" Label
+//     // (which functions as a placeholder)
+//     if (el.input.value.length > 0) {
+//       el.searchLabel.classList.add("hidden");
+//     } else {
+//       el.searchLabel.classList.remove("hidden");
+//     }
+
+//     // run a search if the search term is longer than
+//     // the minimum set in the config
+//     if (el.input.value.length >= config.minSearchLength) {
+//       const initial = index.search(searchInput.value);
+
+//       // if there are no results, try again with a wildcard,
+//       // and then yet again with two wildcards
+//       if (initial.length === 0) {
+//         const wildcardResults = index.search(`${searchInput.value}*`);
+//         state.results.full =
+//           wildcardResults.length > 0
+//             ? wildcardResults
+//             : index.search(`*${searchInput.value}*`);
+//       } else {
+//         state.results.full = initial;
+//       }
+
+//       displayResults();
+//     }
+//   }
+
+//   el.input.addEventListener("input", search);
+//   el.close.addEventListener("click", hideSearchBox);
+// }
+
+// document.addEventListener("DOMContentLoaded", initializeSearch);
--- docs/assets/js/ajax.js
+++ docs/assets/js/ajax.js
@@ -1,32 +1,99 @@
-async function getDocument(url) {
+// Constants
+const CONTENT_CONTAINER_ID = "content";
+const PARSER = new DOMParser();
+
+// State
+let historyStack = [];
+let currentStateIndex = -1;
+
+// Functions
+async function fetchDocument(url) {
   try {
     const response = await fetch(url);
-    const html = await response.text();
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(html, "text/html");
-    const contentDiv = htmlDoc.getElementById("content");
-    const contentHtml = contentDiv.outerHTML;
-    document.getElementById("content").innerHTML = contentDiv.innerHTML;
+    return await response.text();
   } catch (error) {
-    throw new Error("Could not fetch page.", { cause: error });
+    throw new Error(`Failed to fetch document: ${url}`, { cause: error });
   }
+}
+
+async function parseHtml(html) {
+  return PARSER.parseFromString(html, "text/html");
+}
+
+async function updateContent(htmlDoc) {
+  const title = htmlDoc.querySelector("title").textContent;
+  const contentDiv = htmlDoc.getElementById(CONTENT_CONTAINER_ID);
+  const target = document.getElementById(CONTENT_CONTAINER_ID);
+  target.innerHTML = contentDiv.innerHTML;
+  target.scrollTop = 0;
+  document.title = title;
+
+  // Reinitialize link click events for newly loaded content
+  createLinkClickEvents(target);
+}
+
+function pushToHistory(url) {
+  historyStack = historyStack.slice(0, currentStateIndex + 1);
+  historyStack.push(url);
+  currentStateIndex++;
+  history.pushState({ index: currentStateIndex }, "", url);
+}
+
+async function navigate(url, addToHistory = true) {
+  const urlObj = new URL(url, window.location.origin);
+
+  if (urlObj)
+    if (urlObj.hash && urlObj.pathname === window.location.pathname) {
+      // Handle local page anchor navigation
+      const anchor = document.querySelector(urlObj.hash);
+      if (anchor) {
+        anchor.scrollIntoView();
+        addToHistory && pushToHistory(url);
+        return;
+      }
+    }
+
+  // Handle Local Navigation
+  const html = await fetchDocument(url);
+  const htmlDoc = await parseHtml(html);
+  await updateContent(htmlDoc);
+
+  addToHistory && pushToHistory(url);
 }
 
 function createLinkClickEvents(root) {
-  const elements = root.querySelectorAll("a");
-  console.log(elements);
-  if (elements.length) {
-    elements.forEach((link) => {
-      link.addEventListener("click", (e) => {
+  const links = root.querySelectorAll("a");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const url = e.currentTarget.href;
+      if (isLocalLink(url)) {
         e.preventDefault();
-        getDocument(e.currentTarget.href);
-      });
+        navigate(url);
+      }
     });
+  });
+}
+
+function isLocalLink(url) {
+  const linkUrl = new URL(url, window.location.origin);
+  return linkUrl.origin === window.location.origin;
+}
+
+window.addEventListener("popstate", (e) => {
+  if (e.state && e.state.index !== undefined) {
+    currentStateIndex = e.state.index;
+    navigate(historyStack[currentStateIndex], false);
   }
-}
+});
 
-function ajax() {
+// Initialize
+function initAjax() {
   createLinkClickEvents(document);
+  // Initialize the first state
+  historyStack.push(window.location.href);
+  currentStateIndex++;
+  history.replaceState({ index: currentStateIndex }, "", window.location.href);
 }
 
-document.addEventListener("DOMContentLoaded", ajax);
+// Call init
+document.addEventListener("DOMContentLoaded", initAjax);
