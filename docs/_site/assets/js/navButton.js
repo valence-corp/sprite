f75306903e0b52ddd224575afc7346a74b0485ea
Your Name <you@example.com>
2024-06-23 20:27:24 -0600

    fix nav in docs

--- docs/_site/assets/js/navButton.js
+++ docs/_site/assets/js/navButton.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("navButton");
-  const desktopButton = document.getElementById("navToggleDesktop");
+  const button = document.getElementById("nav_button");
   const indicator = button.querySelector("svg");
   const nav = document.getElementById("mainNavigation");
   let open = false;
-
-  desktopButton.addEventListener("click", function () {
-    nav.classList.toggle("open");
-  });
 
   button.addEventListener("click", function () {
     nav.classList.toggle("open");
@@ -19,8 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
       `;
     } else {
       indicator.innerHTML = `
-        <line x1="34" y1="30" x2="18" y2="30"></line>
-        <line x1="18" y1="22" x2="34" y2="22"></line>
+        <line x1="18" y1="20" x2="34" y2="20"></line>
+        <line x1="18" y1="26" x2="34" y2="26"></line>
+        <line x1="18" y1="32" x2="34" y2="32"></line>
       `;
     }
   });
