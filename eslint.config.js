eb8164d715b0f47f5d0f4f474976afec6799d1f1
Your Name <you@example.com>
2024-06-27 07:34:13 -0600

    prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-// @ts-check
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-  eslint.configs.recommended,
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-);
+];
