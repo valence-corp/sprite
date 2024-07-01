d78552ef1e95a39a01517a365629cca3312b3296
Your Name <you@example.com>
2024-06-30 18:48:40 -0600

    query integration test

--- test/integration/database/query.test.ts
+++ test/integration/database/query.test.ts
@@ -1,13 +1,21 @@
 import { ArcadeSupportedQueryLanguages } from '../../../src/types/database.js';
 import { testClient as client } from './testClient.js';
 
+type DBSettings = {
+  dateFormat: string;
+  dateTimeFormat: string;
+  encoding: string;
+};
+
 describe('SpriteDatabase.query', () => {
   it('executes a query successfully', async () => {
     const command = 'SELECT FROM schema:database';
     const language: ArcadeSupportedQueryLanguages = 'sql';
-    const result = await client.query(language, command);
+    const result: Array<DBSettings> = await client.query(language, command);
 
-    expect(result).toMatchSnapshot();
+    expect(result[0].dateFormat).toBeDefined();
+    expect(result[0].dateTimeFormat).toBeDefined();
+    expect(result[0].encoding).toBeDefined();
   });
 
   it('propagates errors from ArcadeDB', async () => {
