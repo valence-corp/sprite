d86dd6038395c88ba35fff1ea7ca1784270c8378
Your Name <you@example.com>
2024-07-01 18:57:45 -0600

    insert nodes unit tests

--- test/unit/database/dialect/nodes/insert/bucket.ts
+++ test/unit/database/dialect/nodes/insert/bucket.ts
@@ -1,3 +1,13 @@
-// export function bucket(bucketName: string) {
-//     return `BUCKET:${bucketName}`;
-// }
+import { bucket } from '@/nodes/insert/bucket';
+
+describe('sql > nodes > insert > bucket', () => {
+  // export function bucket(bucketName: string) {
+  //     return `BUCKET:${bucketName}`;
+  // }
+  it('should return a formatted BUCKET string with the provided bucket name', () => {
+    const name = `myBucket`;
+    const expectedResult = `BUCKET:${name}`;
+    const result = bucket(name);
+    expect(result).toEqual(expectedResult);
+  });
+});
