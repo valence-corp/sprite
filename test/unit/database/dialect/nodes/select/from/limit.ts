e2c5434d3a664cb86ade576c3a3e0f2e0f8c3550
Your Name <you@example.com>
2024-07-01 19:23:45 -0600

    unit tests for select from nodes

---
+++ test/unit/database/dialect/nodes/select/from/limit.ts
@@ -0,0 +1,20 @@
+// export function limit(records: number) {
+//   return `LIMIT ${records}`;
+// }
+
+import { limit } from '@/nodes/select/from/limit';
+
+describe('sql > nodes > select > from > limit', () => {
+  it('it should return the LIMIT + string input', () => {
+    const input = 1;
+    const result = limit(input);
+
+    expect(result).toBe(`LIMIT ${input}`);
+  });
+  it('it should handle different inputs', () => {
+    const input = 2;
+    const result = limit(input);
+
+    expect(result).toBe(`LIMIT ${input}`);
+  });
+});
